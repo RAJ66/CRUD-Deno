fdf056753be68ebb1d6d76ec21f0b93f0d974094
Vitor <vitorbarbosa1998@hotmail.com>
2020-09-03 15:11:10 +0100

    fix :deploy

--- api/app.ts
+++ api/app.ts
@@ -1,9 +1,16 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 import router from "./routes/routes.ts";
+import * as flags from "https://deno.land/std/flags/mod.ts";
+
+const { args, exit } = Deno;
+const argPort = parse(args).port;
 
 const HOST = config().HOST ?? "127.0.0.1";
 const PORT = config().PORT ?? 8080;
+
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : PORT;
 
 const app = new Application();
 
@@ -11,4 +18,5 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log(`Deno is running: ${HOST}:${PORT}`);
-await app.listen(`${HOST}:${PORT}`);
+//await app.listen(`${HOST}:${PORT}`);
+await app.listen({ port: port });
