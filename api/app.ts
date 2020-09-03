df85d9a27b43a83a9e2448b2b29a9667e50b9c0f
Vitor <vitorbarbosa1998@hotmail.com>
2020-09-03 15:35:38 +0100

    test router

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import * as flags from "https://deno.land/std/flags/mod.ts";
 import router from "./routes/routes.ts";
 
@@ -8,8 +8,14 @@ const argPort = flags.parse(args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
 const app = new Application();
+const route = new Router();
+route.get("/", ({ response }: { response: any }) => {
+  response.body = "test";
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(route.routes());
+app.use(route.allowedMethods());
 
 await app.listen({ port });
--- api/routes/routes.ts
+++ api/routes/routes.ts
@@ -2,7 +2,7 @@ import { Router } from "https://deno.land/x/oak/mod.ts";
 
 const router = new Router();
 
-router.get("/api/v1/status", (context) => {
+router.get("/api/v1/status", ({ context }: { context: any }) => {
   context.response.status = 200;
   context.response.body = "OK";
 });
