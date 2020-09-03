591c849d7dd6cf1955ec13f8ef997a2a777e103c
Vitor <vitorbarbosa1998@hotmail.com>
2020-09-03 15:26:38 +0100

    fix

--- api/app.ts
+++ api/app.ts
@@ -3,9 +3,6 @@ import * as flags from "https://deno.land/std/flags/mod.ts";
 import router from "./routes/routes.ts";
 
 const { args } = Deno;
-const argPort = parse(args).port;
-
-const { args } = Deno;
 const DEFAULT_PORT = 8080;
 const argPort = flags.parse(args).port;
 const port = argPort ? Number(argPort) : DEFAULT_PORT;
@@ -15,5 +12,4 @@ const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-console.log(`Deno is running: ${port}`);
 await app.listen({ port });
